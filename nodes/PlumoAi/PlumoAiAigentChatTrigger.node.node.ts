cbe86271b9238638a8d611daa77dc74a5c9fa659
HussainRafiq <71262197+HussainRafiq@users.noreply.github.com>
2025-11-06 13:24:46 +0500

    fix

---
+++ nodes/PlumoAi/PlumoAiAigentChatTrigger.node.node.ts
@@ -0,0 +1,68 @@
+import { NodeConnectionTypes, type INodeType, type INodeTypeDescription } from 'n8n-workflow';
+
+
+export class PlumoAiAigentChatTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'PlumoAI Aigent Chat Trigger',
+		name: 'plumoAiAigentChatTrigger',
+		icon: { light: 'file:../../icons/plumoai.png', dark: 'file:../../icons/plumoai.dark.png' },
+		group: ['input'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Trigger a PlumoAI Aigent Chat',
+		defaults: {
+			name: 'PlumoAI Aigent Chat Trigger',
+		},
+		usableAsTool: true,
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		credentials: [
+			{
+				name: 'plumoaiApi',
+				required: true,
+				displayOptions: {
+					show: {
+						authentication: ['apiToken' ],
+					},
+				},
+			}
+			
+		],
+		requestDefaults: {
+			baseURL: 'https://api.github.cm',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		properties: [
+			{
+				displayName: 'Authentication',
+				name: 'authentication',
+				type: 'options',
+				options: [
+					{
+						name: 'Access Token',
+						value: 'accessToken',
+					},					
+				],
+				default: 'accessToken',
+			},
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					
+				],
+				default: 'issue',
+			},
+			
+		],
+	};
+
+	methods = {
+		
+	};
+}

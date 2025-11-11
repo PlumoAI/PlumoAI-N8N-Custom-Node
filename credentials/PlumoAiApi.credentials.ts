6e37778395576196b8f86aa150f000c6e7e960ff
HussainRafiq <71262197+HussainRafiq@users.noreply.github.com>
2025-11-12 01:38:43 +0500

    fix

---
+++ credentials/PlumoAiApi.credentials.ts
@@ -0,0 +1,57 @@
+import type {
+	Icon,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestOptions,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class PlumoAiApi implements ICredentialType {
+	name = 'plumoAiApi';
+
+	displayName = 'PlumoAI Credential';
+	icon: Icon = 'file:../icons/plumoai.png';
+	documentationUrl = 'https://plumoai.com/docs';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Access Token',
+			name: 'accessToken',
+			type: 'string',
+			typeOptions: { password: true },
+			description: 'The access token of your PlumoAI work environment',
+			placeholder: 'Access Token',
+			default: '',
+		},
+	];
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+		requestOptions.headers = requestOptions.headers ?? {};
+		requestOptions.headers['Authorization'] = `Bearer ${credentials.accessToken}`;
+		return requestOptions;
+	}
+
+	test: ICredentialTestRequest = {
+		request: {
+			url: 'https://api.plumoai.com/Auth/oauth/me',
+			method: 'GET',
+			headers: {
+				Authorization: '=Bearer {{$credentials.accessToken}}',
+			},
+		},
+		rules: [
+			{
+				type: 'responseSuccessBody',
+				properties: {
+					key: 'aud',
+					message: 'Success',
+					value: 'PlumoAI.com',
+				},
+			},
+		],
+	};
+}
